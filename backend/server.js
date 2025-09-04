52fb8cc080f83a10522ae128e73280c9a63b7a52
Abhijeet Ugale <ugaleabhijeet03@gmail.com>
2025-09-05 00:31:15 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,4 @@
 
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// require("dotenv").config();
-
-// const authRoutes = require("./routes/auth");
-
-// const transactionRoutes = require("./routes/transactions");
-// app.use("/api/transactions", transactionRoutes);
-
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
-// // ✅ Test route
-// app.get("/api/test", (req, res) => {
-//   res.json({ message: "Backend is working!" });
-// });
-
-// // Auth routes
-// app.use("/api/auth", authRoutes);
-
-// // Protected example route
-// app.get("/api/protected", require("./middleware/auth"), (req, res) => {
-//   res.json({ message: "You accessed a protected route!", user: req.user });
-// });
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("✅ MongoDB connected");
-//     app.listen(5000, () => console.log("✅ Server running on port 5000"));
-//   })
-//   .catch(err => console.error("MongoDB error:", err));
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
